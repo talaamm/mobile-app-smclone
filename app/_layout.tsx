c9c5f35b1d8695865168006b254d6962226d259c
tala <tala.aunv@gmail.com>
2025-08-08 21:33:52 +0300

    improved ui

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,15 @@
 import { Stack } from 'expo-router';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+
 
 export default function RootLayout() {
   return (
     <Stack initialRouteName="login">
-      <Stack.Screen name="login" options={{ title: 'Log In' }} />
-      <Stack.Screen name="signup" options={{ title: 'Sign Up' }} />
+      <Stack.Screen name="login" options={{ title: 'Log In', headerShown: false }} />
+      <Stack.Screen name="signup" options={{ title: 'Sign Up', headerShown: false }} />
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <StatusBar hidden={true} />
     </Stack>
   );
 } 
